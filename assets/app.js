cd9428c824bc1aeb21945973397ac9911d68131d
Laurent Lassallette <laurent.last7@gmail.com>
2025-11-22 00:54:11 +0100

    add minified css and js

--- assets/app.js
+++ assets/app.js
@@ -1,27 +1,8 @@
-import EventManager from "./js/component/event_manager.js";
-import Ajax from "./js/component/ajax.js";
-import Alert from "./js/component/alert.js";
-import Aside from "./js/component/aside.js";
-import Collapse from "./js/component/collapse.js";
-import Dialog from "./js/component/dialog.js";
-import Dropdown from "./js/component/dropdown.js";
-import Tab from "./js/component/tab.js";
-
 import SyntaxHighlighter from "./js/custom/highlighter.js";
 import { copyCode } from "./js/custom/copy_code.js";
-
 
 window.copyCode = copyCode;
 
 document.addEventListener('DOMContentLoaded', () => {
-    new Ajax();
-    new Alert();
-    new Aside();
-    new Collapse();
-    new Dialog();
-    new Dropdown();
-    new Tab();
-
     new SyntaxHighlighter();
-    console.log(EventManager.listeners);
 });
